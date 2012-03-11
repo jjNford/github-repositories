6a8f872668e4a35aa281f5576d213c6876cc7262
JJ Ford <jj.n.ford@gmail.com>
2012-03-11 13:48:35 -0500

    Remove anonymous function.

--- js/app.js
+++ js/app.js
@@ -1,108 +1,103 @@
-(function() {
-	
-	window.App = {
+window.App = {
 
-		/**
-		 * Initializes the application and the applications components.
-		 */
-		init: function(){
-			Authentication.validate(function(user) {
+	/**
+	 * Initializes the application and the applications components.
+	 */
+	init: function(){
+		Authentication.validate(function(user) {
 
-				User.init(user);	
-				Content.init();
-				App.navigation.init();
-				App.settings.init();
-				App.switcher.init();
-				App.bind();	
+			User.init(user);	
+			Content.init();
+			App.navigation.init();
+			App.settings.init();
+			App.switcher.init();
+			App.bind();	
+			jQuery('body').removeClass('loading').find('#application').show();
 
-				jQuery('body').removeClass('loading').find('#application').show();
-	
-			}, function() {
-				Authentication.prompt();
-			});
-		},
-	
-		/**
-		 * Bind
-		 */
-		bind: function() {
-	
-			// Get notifications.
-			Socket.postMessage("window", "Notifier", "update", []);
-	
-			// Create user link tooltips.
-			jQuery('.user_links [tooltip]').each(function() {
-				var element = jQuery(this);
-				var html = "<span class='tooltip'>"
-				         + "<span class='arrow'></span>"
-						 + "<span class='bubble'>" + element.attr('tooltip') + "</span>"
-						 + "</span>";
-	
-				jQuery(this).append(html);
+		}, function() {
+			Authentication.prompt();
+		});
+	},
 
-				element.on('hover', function() {
-					element.find('.tooltip').fadeToggle(75);
-					var bubble = element.find('.bubble');
-					bubble.css('margin-left', -bubble.width() / 2 + "px");
-				});
-			});
+	/**
+	 * Bind
+	 */
+	bind: function() {
 
-			// Set notification click events.
-			jQuery('.user_links li[rel="notifications"]').on('click', function(event) {
-				event.preventDefault();
-				event.stopPropagation();
-				window.open("https://github.com/inbox/notifications", '_blank');
-			});
-	
-			// Set log out click events.
-			jQuery('.user_links li[rel="log_out"]').on('click', function() {
-				Storage.clear();
-				App.close();
-			});
-	
-			// Set refresh button mouse and click events.
-			var refresh = jQuery('.refresh');
-			refresh.on('click', function() {
-				window[App.navigation.selected].load.refresh(User.context, App.navigation.selected);
-			});
-			refresh.on('mousedown', function() {
-				refresh.addClass('down');
-			});
-			refresh.on('mouseup', function() {
-				refresh.removeClass('down');
-			});
-			refresh.on('mouseleave', function() {
-				refresh.removeClass('down');
-			});
-		},
-	
-		/**
-		 * Close
-		 */
-		close: function(){
-			window.close();
-			chrome.tabs.getCurrent(function(tab) {
-				chrome.tabs.remove(tab.id, function(){});
-			});
-		},
-	
-		/**
-		 * Update
-		 */
-		update: function(count) {
-			var notifications = jQuery('.user_links li[rel="notifications"]');
+		// Get notifications.
+		Socket.postMessage("window", "Notifier", "update", []);
 
-			if(count != '') {
-				var html = "<span class='count'>" + count + "</span>";
-				notifications.append(html);
-			}
-			else {
-				notifications.html("");
-			}
+		// Create user link tooltips.
+		jQuery('.user_links [tooltip]').each(function() {
+			var element = jQuery(this);
+			var html = "<span class='tooltip'>"
+			         + "<span class='arrow'></span>"
+					 + "<span class='bubble'>" + element.attr('tooltip') + "</span>"
+					 + "</span>";
+
+			jQuery(this).append(html);
+
+			element.on('hover', function() {
+				element.find('.tooltip').fadeToggle(75);
+				var bubble = element.find('.bubble');
+				bubble.css('margin-left', -bubble.width() / 2 + "px");
+			});
+		});
+
+		// Set notification click events.
+		jQuery('.user_links li[rel="notifications"]').on('click', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			window.open("https://github.com/inbox/notifications", '_blank');
+		});
+
+		// Set log out click events.
+		jQuery('.user_links li[rel="log_out"]').on('click', function() {
+			Storage.clear();
+			App.close();
+		});
+
+		// Set refresh button mouse and click events.
+		var refresh = jQuery('.refresh');
+		refresh.on('click', function() {
+			window[App.navigation.selected].load.refresh(User.context, App.navigation.selected);
+		});
+		refresh.on('mousedown', function() {
+			refresh.addClass('down');
+		});
+		refresh.on('mouseup', function() {
+			refresh.removeClass('down');
+		});
+		refresh.on('mouseleave', function() {
+			refresh.removeClass('down');
+		});
+	},
+
+	/**
+	 * Close
+	 */
+	close: function(){
+		window.close();
+		chrome.tabs.getCurrent(function(tab) {
+			chrome.tabs.remove(tab.id, function(){});
+		});
+	},
+
+	/**
+	 * Update
+	 */
+	update: function(count) {
+		var notifications = jQuery('.user_links li[rel="notifications"]');
+
+		if(count != '') {
+			var html = "<span class='count'>" + count + "</span>";
+			notifications.append(html);
 		}
-	};
-	
-})();
+		else {
+			notifications.html("");
+		}
+	}
+};
 
 // Prevent any future conflicts.
 $.noConflict();
