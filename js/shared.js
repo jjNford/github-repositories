453dec5681a25c2974290f8350d8dfd47d94c03c
JJ Ford <jj.n.ford@gmail.com>
2012-03-11 13:50:19 -0500

    Add caching, context, and pref shared vars.

--- js/shared.js
+++ js/shared.js
@@ -1,6 +1,11 @@
 // All constants can be added to the shared namespace and used for both the extension, and background page.
-
 window.Shared = {
 
-	CONTEXT_KEY: "context",
+	CACHE_KEY:      "cache.",
+	CACHE_THRESH:   1,
+	CACHE_TTL:      900000,
+	CONTEXT_KEY:    "context",
+	PREF_CACHING:   "pref.caching",
+	PREF_SMART:     "pref.smart",
+	USER_LOGIN:     "login"
 };
