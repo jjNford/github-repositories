4ae5f268d12271dab64bafcc5047b26d123cd793
JJ Ford <jj.n.ford@gmail.com>
2012-03-11 10:18:33 -0500

    All authentication to the global namespace.

--- js/app.js
+++ js/app.js
@@ -1,115 +1,122 @@
-window.App = {
+(function() {
+	
+	window.App = {
 
-	/**
-	 * Initializes the application and the applications components.
-	 */
-	init: function(){
-		App.authentication.validate(function(user) {
+		/**
+		 * Initializes the application and the applications components.
+		 */
+		init: function(){
+			Authentication.validate(function(user) {
 
-			// Merge user data from GitHub with user object and initialize user.
-			jQuery.extend(User, user);
-			User.init();
+				// Merge user data from GitHub with user object and initialize user.
+				jQuery.extend(User, user);
+				User.init();
 	
-			App.content.init();
-			App.navigation.init();
-			App.settings.init();
-			App.switcher.init();
+				// Initialize components.
+				App.content.init();
+				App.navigation.init();
+				App.settings.init();
+				App.switcher.init();
 	
-			App.bind();	
+				App.bind();	
 
-			// Show application.
-			jQuery('body').removeClass('loading').find('#application').show();
+				// Show application.
+				jQuery('body').removeClass('loading').find('#application').show();
 	
-		}, function() {
-			App.authentication.prompt();
-		});
-	},
-	
-	/**
-	 * Bind
-	 */
-	bind: function() {
-	
-		// Get notifications.
-		Socket.postMessage("window", "Notifier", "update", []);
-	
-		// Create user link tooltips.
-		jQuery('.user_links [tooltip]').each(function() {
-			var element = jQuery(this);
-			var html = "<span class='tooltip'>"
-			         + "<span class='arrow'></span>"
-					 + "<span class='bubble'>" + element.attr('tooltip') + "</span>"
-					 + "</span>";
-	
-			jQuery(this).append(html);
-
-			element.on('hover', function() {
-				element.find('.tooltip').fadeToggle(75);
-				var bubble = element.find('.bubble');
-				bubble.css('margin-left', -bubble.width() / 2 + "px");
+			}, function() {
+				Authentication.prompt();
 			});
-		});
+		},
+	
+		/**
+		 * Bind
+		 */
+		bind: function() {
+	
+			// Get notifications.
+			Socket.postMessage("window", "Notifier", "update", []);
+	
+			// Create user link tooltips.
+			jQuery('.user_links [tooltip]').each(function() {
+				var element = jQuery(this);
+				var html = "<span class='tooltip'>"
+				         + "<span class='arrow'></span>"
+						 + "<span class='bubble'>" + element.attr('tooltip') + "</span>"
+						 + "</span>";
+	
+				jQuery(this).append(html);
 
-		// Set notification click events.
-		jQuery('.user_links li[rel="notifications"]').on('click', function(event) {
-			event.preventDefault();
-			event.stopPropagation();
-			window.open("https://github.com/inbox/notifications", '_blank');
-		});
-	
-		// Set log out click events.
-		jQuery('.user_links li[rel="log_out"]').on('click', function() {
-			Storage.clear();
-			App.close();
-		});
-	
-		// Set refresh button mouse and click events.
-		var refresh = jQuery('.refresh');
-		refresh.on('click', function() {
-			window[App.navigation.selected].load.refresh(User.context, App.navigation.selected);
-		});
-		refresh.on('mousedown', function() {
-			refresh.addClass('down');
-		});
-		refresh.on('mouseup', function() {
-			refresh.removeClass('down');
-		});
-		refresh.on('mouseleave', function() {
-			refresh.removeClass('down');
-		});
-	},
-	
-	/**
-	 * Close
-	 */
-	close: function(){
-		window.close();
-		chrome.tabs.getCurrent(function(tab) {
-			chrome.tabs.remove(tab.id, function(){});
-		});
-	},
-	
-	/**
-	 * Update
-	 */
-	update: function(count) {
-		var notifications = jQuery('.user_links li[rel="notifications"]');
+				element.on('hover', function() {
+					element.find('.tooltip').fadeToggle(75);
+					var bubble = element.find('.bubble');
+					bubble.css('margin-left', -bubble.width() / 2 + "px");
+				});
+			});
 
-		if(count != '') {
-			var html = "<span class='count'>" + count + "</span>";
-			notifications.append(html);
+			// Set notification click events.
+			jQuery('.user_links li[rel="notifications"]').on('click', function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+				window.open("https://github.com/inbox/notifications", '_blank');
+			});
+	
+			// Set log out click events.
+			jQuery('.user_links li[rel="log_out"]').on('click', function() {
+				Storage.clear();
+				App.close();
+			});
+	
+			// Set refresh button mouse and click events.
+			var refresh = jQuery('.refresh');
+			refresh.on('click', function() {
+				window[App.navigation.selected].load.refresh(User.context, App.navigation.selected);
+			});
+			refresh.on('mousedown', function() {
+				refresh.addClass('down');
+			});
+			refresh.on('mouseup', function() {
+				refresh.removeClass('down');
+			});
+			refresh.on('mouseleave', function() {
+				refresh.removeClass('down');
+			});
+		},
+	
+		/**
+		 * Close
+		 */
+		close: function(){
+			window.close();
+			chrome.tabs.getCurrent(function(tab) {
+				chrome.tabs.remove(tab.id, function(){});
+			});
+		},
+	
+		/**
+		 * Update
+		 */
+		update: function(count) {
+			var notifications = jQuery('.user_links li[rel="notifications"]');
+
+			if(count != '') {
+				var html = "<span class='count'>" + count + "</span>";
+				notifications.append(html);
+			}
+			else {
+				notifications.html("");
+			}
 		}
-		else {
-			notifications.html("");
-		}
-	}
-};
+	};
+	
+})();
 
 // Prevent any future conflicts.
 $.noConflict();
 
 // Initialize application after page has loaded.
 jQuery(window).bind("load", function() {
+	
+	// Kill the extension if local storage is not supported.
 	if(!Storage.isSupported()) {
 		App.close();
 	}
--- js/authentication.js
+++ js/authentication.js
@@ -1,57 +1,63 @@
-window.App.authentication = {
-	
-	/**
-	 * Validate
-	 * 
-	 * @param pass Callback to be executed if validation is successful, sent user parameter.
-	 * @param fail Callback to be executed if validation fails.
-	 */
-	validate: function(pass, fail) {
-		if(OAuth2.getToken() == null) {
-			fail();
-		}
-		else {
-			jQuery.getJSON("https://api.github.com/user", {access_token: OAuth2.getToken()})
-				.success(function(json) {
-					if(json.type == "User") {
-	
-						var user = {};
-						user.logged = json;
-	
-						// Load user's organizations.
-						jQuery.getJSON("https://api.github.com/user/orgs", {access_token: OAuth2.getToken()})
-							.success(function(json) {
-	
-								// Save user name for notifier to use.
-								Storage.save("login", user.logged.login);
+(function() {
 
-								user.orgs = json;
-								pass(user);
-							});
-					}
-				})
-				.error(function(json) {
-					if(json.readyState == 0 && json.status == 0) {
-						// There is no data connection.
-					}
-					else {
-						fail();
-					}
-				});
-		}
-	},
+	window.Authentication = {
 	
-	/**
-	 * Prompt
-	 */
-	prompt: function() {
-		jQuery('.github_header').delay(500).fadeOut(200, function() {
-			jQuery('body').removeClass('loading').animate({width:"413px", height:"269px"}, function() {
-				jQuery('#authorization').delay(750).fadeIn(225);
-				jQuery('#authorization button').on('click', function() {
-					OAuth2.begin();
+		/**
+		 * Validate that extension still has permission to access users private data.
+		 * 
+		 * @param success Callback to be executed if validation is successful, sent user parameter.
+		 * @param error Callback to be executed if validation fails.
+		 */
+		validate: function(success, error) {
+			if(OAuth2.getToken() == null) {
+				error();
+			}
+			else {
+
+				// To validate user, load user object from GitHub.
+				jQuery.getJSON("https://api.github.com/user", {access_token: OAuth2.getToken()})
+					.success(function(json) {
+						if(json.type == "User") {
+	
+							var user = {};
+							user.logged = json;
+	
+							// Load user's organizations.
+							jQuery.getJSON("https://api.github.com/user/orgs", {access_token: OAuth2.getToken()})
+								.success(function(json) {
+	
+									// Save user name for notifier to use.
+									Storage.save("login", user.logged.login);
+
+									user.orgs = json;
+									success(user);
+								});
+						}
+					})
+					.error(function(json) {
+						if(json.readyState == 0 && json.status == 0) {
+							// There is no data connection.
+						}
+						else {
+							error();
+						}
+					});
+			}
+		},
+	
+		/**
+		 * Prompt for extension to be authorized to access private GitHub data.
+		 */
+		prompt: function() {
+			jQuery('.github_header').delay(500).fadeOut(200, function() {
+				jQuery('body').removeClass('loading').animate({width:"413px", height:"269px"}, function() {
+					jQuery('#authorization').delay(750).fadeIn(225);
+					jQuery('#authorization button').on('click', function() {
+						OAuth2.begin();
+					});
 				});
 			});
-		});
-	}
-};
+		}
+	};
+
+})();
