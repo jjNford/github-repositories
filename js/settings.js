d3a091b13f6c71cc5aeec5bf2244b5252d8bfd60
JJ Ford <jj.n.ford@gmail.com>
2012-03-13 15:29:36 -0500

    Add Twitter Follow button.

--- js/settings.js
+++ js/settings.js
@@ -51,12 +51,15 @@ window.Settings = {
 				Settings.panel.slideUp(500, function() {
 					Content.article.css('overflow-y', 'auto');
 				});
-				Settings.button.removeClass('active');
+				Settings.button.removeClass('active');				
 			}
 			else {
 				Settings.button.addClass('active');
 				Content.article.css('overflow-y', 'hidden');
 				Settings.panel.slideDown(500);
+				
+				// Render the twitter button.
+				(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}})(document,"script","twitter-wjs");				
 			}
 		});
 	
